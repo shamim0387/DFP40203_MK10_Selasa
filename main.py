028a8fa507918e9d1ed66af3ea388d3d7c1cf67d
shamim0387 <144081099+shamim0387@users.noreply.github.com>
2023-11-07 11:41:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,23 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import glob
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def main():
+    filelist = glob.glob("../DFP40203_MK10_Selasa/*.txt")
+    for filename in filelist:
+        print(filename)
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
